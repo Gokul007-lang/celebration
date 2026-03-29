8555f753fd106d8f5bc7acfed42da7d21e1619f4
Gokul007-lang <gokulvss123@gmail.com>
2026-03-29 22:58:58 +0530

    fixed auth

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 /* 🔥 CONNECT TO MONGODB */
-mongoose.connect("mongodb+srv://Gokul:NOTU@2340@cluster0.fp4hxwy.mongodb.net/birthday")
+mongoose.connect("mongodb+srv://Gokul:gokul123@cluster0.fp4hxwy.mongodb.net/birthday")
 .then(() => console.log("✅ MongoDB Connected"))
 .catch(err => console.log("❌ Mongo Error:", err));
 
